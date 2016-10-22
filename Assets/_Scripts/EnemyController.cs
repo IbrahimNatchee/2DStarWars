09b25c7829f118023c7507650874d7e0bcbed1a2
inatchee <300767752@cencol.ca>
2016-10-22 16:11:41 -0400

    task 2 and 4

---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Speed {
+	public float minSpeed, maxSpeed;
+}
+
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, yMin, yMax;
+}
+
+
+public class EnemyController : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES
+	public Speed speed;
+	public Boundary boundary;
+   private Transform _transform;
+    public Transform SpawnPoint;
+    // PRIVATE INSTANCE VARIABLES
+    private float _CurrentSpeed;
+	private float _CurrentDrift;
+
+	// Use this for initialization
+	void Start () {
+		this._Reset ();
+        this._transform=this.GetComponent<Transform>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 currentPosition = gameObject.GetComponent<Transform> ().position;
+		currentPosition.y -= this._CurrentSpeed;
+		gameObject.GetComponent<Transform> ().position = currentPosition;
+		
+		// Check bottom boundary
+		if (currentPosition.y <= boundary.yMin) {
+			this._Reset();
+		}
+	}
+
+	// resets the gameObject
+	private void _Reset() {
+		this._CurrentSpeed = Random.Range (speed.minSpeed, speed.maxSpeed);
+		Vector2 resetPosition = new Vector2 (Random.Range(boundary.xMin, boundary.xMax), boundary.yMax);
+		gameObject.GetComponent<Transform> ().position = resetPosition;
+        
+}
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //Destroy(other.gameObject);
+            //Debug.Log("You hit a Tai Fighter");
+            other.transform.position = this.SpawnPoint.position;
+        }
+
+    }
+
+}
---
+++ Assets/_Scripts/PlayerCollider.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollider : MonoBehaviour {
+    /*private Transform _transform;
+    public Transform SpawnPoint;*/
+    
+	// Use this for initialization
+	void Start () {
+        /*this._transform = this.GetComponent<Transform>();
+        Vector2 newPosition = this._transform.position;*/
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this._resetenemy();
+	}
+
+    private void _resetenemy() {
+}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            //Destroy(other.gameObject);
+            Debug.Log("You hit a Tai Fighter");
+           // this._transform.position = this.SpawnPoint.position;
+        }
+
+    }
+    
+}
